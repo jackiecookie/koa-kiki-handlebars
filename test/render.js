dad0eb8723a8c024c8d67209674384cff970f871
jackiecookie <2711002357@qq.com>
2016-09-22 02:51:53 -0700

    render file change

--- test/render.js
+++ test/render.js
@@ -1,33 +1,40 @@
-const render=require('../lib/render');
-const assert=require('assert');
+const render = require('../lib/render');
+const assert = require('assert');
+const co = require('co');
 
 
-describe('render(opt)',function () {
+describe('render(opt)', function () {
 
-   it('render is function',function(){
-       assert.equal(typeof render,'function')
-   });
+    it('render is function', function () {
+        assert.equal(typeof render, 'function')
+    });
 
 
-    it('render(null) is render object',function(){
-        const renderObj=render(null);
+    it('render(null) is render object', function () {
+        const renderObj = render(null);
         assert(renderObj instanceof render);
     })
 });
 
 
-
-describe('renderObject function',function(){
+describe('renderObject function', function () {
 
     before(function () {
-        this.renderObj=render({});
+        this.renderObj = render({});
+    });
+
+    it('#renderView()', function () {
+
     })
 
-    it('#readFile()',function () {
-        const file=this.renderObj.readFile('./test/test.hbs');
-        assert(!!file,'file empty')
-    })
-    
+    it('#readFile()', function () {
+        co(this.renderObj.readFile('./test/test.hbs')).then(function(file){
+            assert(!!file, 'file empty')
+        });
+
+
+    });
+
 })
 
 
