ba9a20397f806ee42a81b1e1c26a60673e6e79ff
jackiecookie <2711002357@qq.com>
2016-09-22 02:23:52 -0700

    render file change

--- test/render.js
+++ test/render.js
@@ -1,9 +1,33 @@
-var render=require('../lib/render');
-var assert=require('assert');
+const render=require('../lib/render');
+const assert=require('assert');
 
 
 describe('render(opt)',function () {
+
    it('render is function',function(){
        assert.equal(typeof render,'function')
-   })
+   });
+
+
+    it('render(null) is render object',function(){
+        var renderObj=render(null);
+        assert(renderObj instanceof render);
+    })
 });
+
+
+
+describe('render object function',function(){
+
+    before(function () {
+        this.renderObj=render({});
+    })
+
+    it('#readFile()',function () {
+        var file=this.renderObj.readFile('./test/test.hbs');
+        assert(!!file,'file empty')
+    })
+    
+})
+
+
